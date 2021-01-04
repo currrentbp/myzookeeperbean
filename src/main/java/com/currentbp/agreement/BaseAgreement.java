e74f2aec2640771d17381c1a00fe4070a657f600
baopan <baopan@xiaomi.com>
2021-01-04 20:37:53 +0800

    sss

--- src/main/java/com/currentbp/agreement/BaseAgreement.java
+++ src/main/java/com/currentbp/agreement/BaseAgreement.java
@@ -11,6 +11,14 @@ public class BaseAgreement {
      * 消息id
      */
     private String id;
+    /**
+     * 消息类型
+     */
+    private int type;
+    /**
+     * 消息体
+     */
+    private String body;
 
     public String getId() {
         return id;
@@ -20,10 +28,28 @@ public class BaseAgreement {
         this.id = id;
     }
 
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
     @Override
     public String toString() {
         return "BaseAgreement{" +
                 "id='" + id + '\'' +
+                ", type=" + type +
+                ", body='" + body + '\'' +
                 '}';
     }
 }
